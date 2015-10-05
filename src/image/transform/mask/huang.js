7f6655e371cc667dad9f6f841478d27064b1d617
kelly <kelly.johana.cordoba@correounivalle.edu.co>
2015-10-05 18:52:20 -0500

    Some errors were corrected

--- src/image/transform/mask/huang.js
+++ src/image/transform/mask/huang.js
@@ -21,7 +21,7 @@ export default function huang(histogram) {
     /* Determine the first non-zero bin */
     first_bin = 0;
     for (let ih = 0; ih < histogram.length; ih++) {
-        if (histogram[ih] != 0) {
+        if (histogram[ih] !== 0) {
             first_bin = ih;
             break;
         }
@@ -30,7 +30,7 @@ export default function huang(histogram) {
     /* Determine the last non-zero bin */
     last_bin = 255;
     for (let ih = 255; ih >= first_bin; ih--) {
-        if (histogram[ih] != 0) {
+        if (histogram[ih] !== 0) {
             last_bin = ih;
             break;
         }
@@ -38,7 +38,7 @@ export default function huang(histogram) {
     term = 1.0 / (last_bin - first_bin);
     let  mu_0 = new Array(256);
     sum_pix = num_pix = 0;
-    for (let ih = first_bin; ih < 256; ih++){
+    for (let ih = first_bin; ih < 256; ih++) {
         sum_pix += ih * histogram[ih];
         num_pix += histogram[ih];
         mu_0[ih] = sum_pix / num_pix;
@@ -46,7 +46,7 @@ export default function huang(histogram) {
 
     let mu_1 = new Array(256);
     sum_pix = num_pix = 0;
-    for (let ih = last_bin; ih > 0; ih--){
+    for (let ih = last_bin; ih > 0; ih--) {
         sum_pix += ih * histogram[ih];
         num_pix += histogram[ih];
         mu_1[ih - 1] = sum_pix /  num_pix;
@@ -55,12 +55,12 @@ export default function huang(histogram) {
     /* Determine the threshold that minimizes the fuzzy entropy*/
     threshold = -1;
     min_ent = Number.MAX_VALUE;
-    for (let it = 0; it < 256; it++){
+    for (let it = 0; it < 256; it++) {
         ent = 0.0;
         for (let ih = 0; ih <= it; ih++) {
             /* Equation (4) in Ref. 1 */
             mu_x = 1.0 / (1.0 + term * Math.abs(ih - mu_0[it]));
-            if (!((mu_x  < 1e-06 ) || ( mu_x > 0.999999))) {
+            if (!((mu_x  < 1e-06) || (mu_x > 0.999999))) {
                 /* Equation (6) & (8) in Ref. 1 */
                 ent += histogram[ih] * (-mu_x * Math.log (mu_x) - (1.0 - mu_x) * Math.log (1.0 - mu_x));
             }
@@ -69,7 +69,7 @@ export default function huang(histogram) {
         for (let ih = it + 1; ih < 256; ih++) {
             /* Equation (4) in Ref. 1 */
             mu_x = 1.0 / (1.0 + term * Math.abs (ih - mu_1[it]));
-            if (!((mu_x  < 1e-06 ) || (mu_x > 0.999999))) {
+            if (!((mu_x  < 1e-06) || (mu_x > 0.999999))) {
                 /* Equation (6) & (8) in Ref. 1 */
                 ent += histogram[ih] * (-mu_x * Math.log (mu_x) - (1.0 - mu_x) * Math.log(1.0 - mu_x));
             }
