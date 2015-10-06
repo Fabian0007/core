9c581aac0159f32512267d5e4d211dc6f149d6e7
fabian0007 <fabian.cano@correounivalle.edu.co>
2015-10-06 16:02:16 -0500

    minimum working

--- src/image/transform/mask/mask.js
+++ src/image/transform/mask/mask.js
@@ -1,6 +1,7 @@
 import Image from '../../image';
 
 import percentile from './percentile';
+import minimum from './minimum';
 import {getThreshold} from '../../../util/converter';
 
 /*
@@ -28,6 +29,9 @@ export default function mask({
             break;
         case 'percentile':
             threshold = percentile(histogram);
+            break;
+        case 'minimum':
+            threshold = minimum(histogram);
             break;
         default:
             throw new Error('mask transform unknown algorithm: ' + algorithm);
